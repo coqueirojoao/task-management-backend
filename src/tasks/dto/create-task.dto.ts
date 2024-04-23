41e02f2d731f9c4e8f645274d5baab705f82e6f4
João Pedro <jaaum.p95@gmail.com>
2024-04-23 11:21:07 -0300

    :sparkles: feat: implementa dto CreateTaskDto

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,13 @@
-export class CreateTaskDto {}
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateTaskDto {
+
+    @IsNotEmpty()
+    @IsString()
+    title: string;
+
+    @IsNotEmpty()
+    @IsString()
+    description: string;
+
+}
